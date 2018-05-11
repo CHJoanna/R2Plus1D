0d7f88b2efbfde58dac00a6703ea2ff5604f6d5f
Joanna Hsu <joannahsu@devgpu202.prn2.facebook.com>
2018-05-11 10:18:34 -0700

    update scripts

--- scripts/create_hmdb51_lmdb.sh
+++ scripts/create_hmdb51_lmdb.sh
@@ -1,7 +1,7 @@
 python data/create_video_db.py \
---list_file=data/list/hmdb51/hmdb51_test_01.csv \
---output_file=/data/users/trandu/datasets/hmdb51_test01
+  --list_file=data/mylist/hmdb51/hmdb51_test_01.csv \
+  --output_file=/data/users/joannahsu/datasets/hmdb51_test01
 
 python data/create_video_db.py \
---list_file=data/list/hmdb51/hmdb51_train_01.csv \
---output_file=/data/users/trandu/datasets/hmdb51_train01
+  --list_file=data/mylist/hmdb51/hmdb51_train_01.csv \
+  --output_file=/data/users/joannahsu/datasets/hmdb51_train01
